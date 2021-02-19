d959a3e300bf25eba762a72dd1d49ce4ecc5bf91
Rene Cutipa <renecutipa@gmail.com>
2021-02-19 17:55:44 -0500

    A

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+  
+use Illuminate\Database\Seeder;
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+  
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+        	'name' => 'Rene Cutipa', 
+        	'email' => 'rcutipa@mdv.pe',
+        	'password' => bcrypt('mdv$3oo')
+        ]);
+  
+        $role = Role::create(['name' => 'Admin']);
+   
+        $permissions = Permission::pluck('id','id')->all();
+  
+        $role->syncPermissions($permissions);
+   
+        $user->assignRole([$role->id]);
+    }
+}
